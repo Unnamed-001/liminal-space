af5bc73a9560668aeb977d24cc7996002fe67453
Unnamed-001 <limebrawl1001@gmail.com>
2026-06-16 18:07:52 -0400

    Me bloquee

--- Scripts/resources/Recurso_ataque.gd
+++ Scripts/resources/Recurso_ataque.gd
@@ -1,11 +1,14 @@
 extends Resource
 class_name AttackDB
 
+enum TYPE { PHYSICAL, PSICOLOGYCAL, DRAINING }
+
 @export_category("info")
 @export_multiline() var name: Dictionary[String, String] = {"ES_CL": "", "EN_US": ""}
 @export var unique_id: int
 @export_category("damage")
 @export var damage: int = 0
-@export var type: GameMaster.type
+@export var type: TYPE = TYPE.PHYSICAL
 @export_range(0.0, 30.0) var variance: float = 0
 @export_range(0.0, 100.0) var critical_chance: float = 10
+@export var effect: EffectDB
